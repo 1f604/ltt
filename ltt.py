0b73b86163b7e8af0fa2b4707c17672ee4baa52d
1f604 <1f604@users.noreply.github.com>
2018-05-30 16:19:15 +0100

    get active application name and window name and role name

--- ltt.py
+++ ltt.py
@@ -1,22 +1,30 @@
 import ctypes
 import os
 from ActivityTracker import ActivityTracker
+import WindowTracker as w
 from time import sleep
 import datetime
+now = datetime.datetime.now
 
 a = ActivityTracker(5)
 
-lastloggedtime = datetime.datetime.now()
 
-def t2s(t): #timedelta to seconds string
-    return str(t.total_seconds()).split(".")[0]
-
-def s2t(s): #seconds string to timedelta
-    return datetime.timedelta(seconds=int(s))
+def download_html(url):
+    pass
 
 def mainloop():
-    for i in range(50000):
-        print datetime.datetime.now(), a.is_user_inactive()
+    while True:
         sleep(0.05) #0.05 seems fast enough to capture the 1 inactivity before suspend, at least on my system.
+        if a.is_user_inactive():
+            print now(), "inactive"
+        else:
+            application = w.get_application()
+            print application.window_title, "|", application.window_instance, "|", application.window_class, "|", application.wm_window_role, "|", application.is_browser
+            if application.is_browser:
+                url = application.url
+                print now(), url
+                download_html(url)
+            elif application.has_document:
+                print now(), application.document
 
 mainloop()
